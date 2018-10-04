aec6b4f522f7532d85d3b1891034a983cdba2e57
michael sadaghyani <sadag004@umn.edu>
2018-10-04 00:41:31 -0500

    Added more stuff

--- food/main.cpp
+++ food/main.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main(){
-    burrito bo;
+    burrito bo; 
     cout << "bo weight is " << bo.getHeight(35) << endl; 
     return 0; 
 }
+
+//burrito* bo; 
+//cout << "bo weight is " << bo -> getHeight(35) << endl; 
